64402e1fe684d0c5aa979b95cbe82a0b4e5a68ca
yahya el hadri <yahyaelhadricgs@gmail.com>
2024-09-12 11:51:04 +0200

    Configuracion Gradle

--- entregas/ElHadri_ElBakkali_Yahya/torneo_tenis/build.gradle.kts
+++ entregas/ElHadri_ElBakkali_Yahya/torneo_tenis/build.gradle.kts
@@ -1,5 +1,15 @@
+val koin_version: String by project
+val logging_version: String by project
+val logback_version: String by project
+
 plugins {
     kotlin("jvm") version "1.9.23"
+    //Dokka documentation
+    id("org.jetbrains.dokka") version "1.9.20"
+    //SqlDelight
+    id("app.cash.sqldelight") version "2.0.2"
+    //Serialization Kotlin
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 group = "org.example"
@@ -11,6 +21,15 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    //Logger
+    implementation("org.lighthousegames:logging:$logging_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    //SqlDelight
+    implementation("app.cash.sqldelight:sqlite-driver:2.0.2")
+    //Serialization JSON
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    // Mock
+    testImplementation("io.mockk:mockk:1.13.10")
 }
 
 tasks.test {
@@ -18,4 +37,20 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+sqldelight {
+    databases {
+        create("AppDatabase") {
+            packageName.set("dev.yahyaelhadri.database")
+        }
+    }
 }
