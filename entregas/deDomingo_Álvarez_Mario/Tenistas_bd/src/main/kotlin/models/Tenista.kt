3ba31cbb6d8bc75215f570e8c004791ffa766b83
Mario de Domingo Alvarez <wolverine.mda.307@gmail.com>
2024-09-13 12:31:15 +0200

    añado tenista

---
+++ entregas/deDomingo_Álvarez_Mario/Tenistas_bd/src/main/kotlin/models/Tenista.kt
@@ -0,0 +1,17 @@
+package org.example.tenistas.models
+
+import java.time.LocalDateTime
+import java.time.LocalDate
+
+data class Tenista(
+    val id: Int = 0,
+    val nombre: String,
+    val pais: String,
+    val altura: Int,
+    val peso: Int,
+    val puntos: Int,
+    val mano: Mano,
+    val fechaNacimiento: LocalDate,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
+)
